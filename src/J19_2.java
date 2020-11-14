95782586b082f9316aa48dca273b60afedc48008
Maciej Wrabel <maciejwrabel@interia.pl>
2020-11-15 00:55:33 +0100

    Optimisation of "if" loops.

--- src/J19_2.java
+++ src/J19_2.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 /*
 2020.04.28 - J19_2 - praca domowa (kółko/krzyżyk)
 Zaprojektuj grę w kółko i krzyżyk
@@ -58,14 +59,7 @@ public class J19_2 {
                         zawodnik = 1;
                     }
                 }
-/*
-            if (zawodnik == 1) {
-                zawodnik = 2;
-            } else {
-                zawodnik = 1;
-            }
 
- */
             } catch (java.util.InputMismatchException execption_1) {
                 System.out.println();
                 System.out.println("Exception_1: Program przyjmuje tylko liczby od 0 do 3 (zakres tablicy 3x3)");
@@ -78,58 +72,30 @@ public class J19_2 {
                 sc.nextLine();
             }
 
-            //sprawdzenie czy wygrał zawodnik nr 1
+            if ((gra[0][0] == 1 && gra[1][0] == 1 && gra[2][0] == 1) ||
+                    (gra[0][1] == 1 && gra[1][1] == 1 && gra[2][1] == 1) ||
+                    (gra[0][2] == 1 && gra[1][2] == 1 && gra[2][2] == 1) ||
+                    (gra[0][0] == 1 && gra[0][1] == 1 && gra[0][2] == 1) ||
+                    (gra[1][0] == 1 && gra[1][1] == 1 && gra[1][2] == 1) ||
+                    (gra[2][0] == 1 && gra[2][1] == 1 && gra[2][2] == 1) ||
+                    (gra[0][0] == 1 && gra[1][1] == 1 && gra[2][2] == 1) ||
+                    (gra[2][0] == 1 && gra[1][1] == 1 && gra[0][2] == 1)) {
 
-            if (gra[0][0] == 1 && gra[1][0] == 1 && gra[2][0] == 1) {
                 System.out.println("Wygrywa zawodnik 1");
                 break;
-            } else if (gra[0][1] == 1 && gra[1][1] == 1 && gra[2][1] == 1) {
-                System.out.println("Wygrywa zawodnik 1");
-                break;
-            } else if (gra[0][2] == 1 && gra[1][2] == 1 && gra[2][2] == 1) {
-                System.out.println("Wygrywa zawodnik 1");
-                break;
-            } else if (gra[0][0] == 1 && gra[0][1] == 1 && gra[0][2] == 1) {
-                System.out.println("Wygrywa zawodnik 1");
-                break;
-            } else if (gra[1][0] == 1 && gra[1][1] == 1 && gra[1][2] == 1) {
-                System.out.println("Wygrywa zawodnik 1");
-                break;
-            } else if (gra[2][0] == 1 && gra[2][1] == 1 && gra[2][2] == 1) {
-                System.out.println("Wygrywa zawodnik 1");
-                break;
-            } else if (gra[0][0] == 1 && gra[1][1] == 1 && gra[2][2] == 1) {
-                System.out.println("Wygrywa zawodnik 1");
-                break;
-            } else if (gra[2][0] == 1 && gra[1][1] == 1 && gra[0][2] == 1) {
-                System.out.println("Wygrywa zawodnik 1");
-                break;
-            }
-            // sprawdzenie czy wygrał zawodnik nr 2
-            else if (gra[0][0] == 2 && gra[1][0] == 2 && gra[2][0] == 2) {
+
+            } else if ((gra[0][0] == 2 && gra[1][0] == 1 && gra[2][0] == 2) ||
+                    (gra[0][1] == 2 && gra[1][1] == 2 && gra[2][1] == 2) ||
+                    (gra[0][2] == 2 && gra[1][2] == 2 && gra[2][2] == 2) ||
+                    (gra[0][0] == 2 && gra[0][1] == 2 && gra[0][2] == 2) ||
+                    (gra[1][0] == 2 && gra[1][1] == 2 && gra[1][2] == 2) ||
+                    (gra[2][0] == 2 && gra[2][1] == 2 && gra[2][2] == 2) ||
+                    (gra[0][0] == 2 && gra[1][1] == 2 && gra[2][2] == 2) ||
+                    (gra[2][0] == 2 && gra[1][1] == 2 && gra[0][2] == 2)) {
+
                 System.out.println("Wygrywa zawodnik 2");
                 break;
-            } else if (gra[0][1] == 2 && gra[1][1] == 2 && gra[2][1] == 2) {
-                System.out.println("Wygrywa zawodnik 2");
-                break;
-            } else if (gra[0][2] == 2 && gra[1][2] == 2 && gra[2][2] == 2) {
-                System.out.println("Wygrywa zawodnik 2");
-                break;
-            } else if (gra[0][0] == 2 && gra[0][1] == 2 && gra[0][2] == 2) {
-                System.out.println("Wygrywa zawodnik 2");
-                break;
-            } else if (gra[1][0] == 2 && gra[1][1] == 2 && gra[1][2] == 2) {
-                System.out.println("Wygrywa zawodnik 2");
-                break;
-            } else if (gra[2][0] == 2 && gra[2][1] == 2 && gra[2][2] == 2) {
-                System.out.println("Wygrywa zawodnik 2");
-                break;
-            } else if (gra[0][0] == 2 && gra[1][1] == 2 && gra[2][2] == 2) {
-                System.out.println("Wygrywa zawodnik 2");
-                break;
-            } else if (gra[2][0] == 2 && gra[1][1] == 2 && gra[0][2] == 2) {
-                System.out.println("Wygrywa zawodnik 2");
-                break;
+
             } else if (gra[0][0] != 0 && gra[0][1] != 0 && gra[0][2] != 0
                     && gra[1][0] != 0 && gra[1][1] != 0 && gra[1][2] != 0
                     && gra[2][0] != 0 && gra[2][1] != 0 && gra[2][2] != 0) {
@@ -149,7 +115,5 @@ public class J19_2 {
 
         System.out.println("KONIEC GRY");
     }
-
 }
-
 
